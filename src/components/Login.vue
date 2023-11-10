c4c5725dcedbd74c6871937b1c3c747b8cfce5f7
Juan Carlos <jclopezniebla@gmail.com>
2023-11-10 03:26:06 -0300

    Refactor code for improved readability and performance.

---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="LoginApp">
+    <el-card class="box-card">
+      <img class="logo" alt="OFPZL Logo" src="./img/Logo.png" />
+      <h3>DeskPuzzle Mail</h3>
+      <el-input
+        type="email"
+        placeholder="email@example.com"
+        v-model="input"
+        clearable
+      ></el-input>
+      <div class="separacion"></div>
+      <el-input
+        type="password"
+        placeholder="Password"
+        v-model="password"
+        clearable
+      >
+      </el-input>
+      <div class="separacion"></div>
+      <el-button class="btn" type="success">Login</el-button>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+      password: "",
+      checked: false,
+    };
+  },
+  name: "LoginApp",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.box-card {
+  width: 480px;
+}
+.LoginApp {
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.logo {
+  width: 30%;
+  height: 30%;
+  border: 50%;
+}
+.btn {
+  margin: 10px;
+}
+.separacion {
+  height: 10px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <navMenu />
+    <LoginApp />
+  </div>
+</template>
+
+<script>
+import LoginApp from "@/components/Login.vue";
+import navMenu from "@/components/navMenu.vue";
+
+export default {
+  name: "LoginApp",
+  components: {
+    LoginApp,
+    navMenu,
+  },
+};
+</script>
